eca2df48d3d0aef35ef7dac98b8bc315d155ea4b
Kohl Braman <kohlbraman@gmail.com>
2023-11-02 09:21:26 -0700

    added to vending machine.

--- src/main/java/com/techelevator/VendingMachineItems/VendingMachine.java
+++ src/main/java/com/techelevator/VendingMachineItems/VendingMachine.java
@@ -1,4 +1,25 @@
 package com.techelevator.VendingMachineItems;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class VendingMachine {
+
+    private Map<String, VendingItem> inventory;
+    public VendingMachine() throws FileNotFoundException {
+
+        File inputFile = new File("vendingmachine.csv");
+        Scanner scanner = new Scanner(inputFile);
+
+        while (scanner.hasNextLine()) {
+            String inLine = scanner.nextLine();
+
+
+            String[] itemArray = inLine.split("\\|");
+        }
+    }
+
 }
