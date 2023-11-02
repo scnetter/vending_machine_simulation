c579d0801ebbd8eb5deeeadb192ee02753e2107d
Kelly Netterville <36lima@gmail.com>
2023-11-02 11:18:40 -0500

    Create attributes and constructor for VendingItem class.

--- src/main/java/com/techelevator/VendingMachineItems/VendingItem.java
+++ src/main/java/com/techelevator/VendingMachineItems/VendingItem.java
@@ -1,4 +1,47 @@
 package com.techelevator.VendingMachineItems;
 
 public class VendingItem {
+    private String name;
+    private double price;
+    private String slot;
+    private int remaining;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getSlot() {
+        return slot;
+    }
+
+    public void setSlot(String slot) {
+        this.slot = slot;
+    }
+
+    public int getRemaining() {
+        return remaining;
+    }
+
+    public void setRemaining(int remaining) {
+        this.remaining = remaining;
+    }
+
+    public VendingItem(String slot, String name, double price){
+        this.slot = slot;
+        this.name = name;
+        this.price = price;
+        this.remaining = 5;
+    }
 }
